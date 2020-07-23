2489a7845db0ea61b796b07104351de5233b9034
Luiz Passos Filho <0509luiz@gmail.com>
2020-07-23 18:37:31 -0300

    validação de formulário

--- app.js
+++ app.js
@@ -1,41 +1,55 @@
 //Carregando módulos
-    const express = require ("express")
-    const handlebars = require("express-handlebars")
-    const bodyParser = require ("body-parser")
-    const mongoose = require("mongoose")
-    const app = express()
-    const path = require("path")
-    var user = require('./routes/user');
+const express = require("express")
+const handlebars = require("express-handlebars")
+const bodyParser = require("body-parser")
+const mongoose = require("mongoose")
+const app = express()
+const path = require("path")
+var user = require('./routes/user');
+const session = require("express-session")
+const flash = require("connect-flash")
 
 
 //Configurações
+    //Sessão
+        app.use(session({
+            secret: "estudodenode",
+            resave: true,
+            saveUnitialized: true
+        }))
+        app.use(flash())
+    //middleware
+        app.use((req, res, next) => {
+            res.locals.success_msg = req.flash("success_msg")
+            res.locals.error_msg + req.flash("error_msg")
+            next()
+        })
     //Body Parse
-        app.use(bodyParser.urlencoded({extended: true}))
+        app.use(bodyParser.urlencoded({ extended: true }))
         app.use(bodyParser.json())
     //HandleBars
         app.engine('handlebars', handlebars({ defaultLayouts: 'main' }))
         app.set('view engine', 'handlebars')
     //mongoose
         mongoose.Promise = global.Promise;
-        mongoose.connect("mongodb://localhost/todoapp", {useNewUrlParser: true, useUnifiedTopology: true}).then(() =>{
+        mongoose.connect("mongodb://localhost/todoapp", { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
             console.log("Conectado com sucesso!")
-        }).catch((err) =>{
+        }).catch((err) => {
             mongoose.Promise = global.Promise;
             console.log("Erro ao conectar: " + err)
         })
     //Public
-    app.use(express.static('public'));
-    
-
+        app.use(express.static('public'));
 
 //Rotas
-app.get("/",(req, res)=>{
-    res.send("olá amigo")})
+    app.get("/", (req, res) => {
+        res.send("olá amigo")
+    })
 
 
 //Outros
-const PORT = 8081;
-app.listen(PORT, ()=>{
-    console.log("Servidor rodando...")
-})
-app.use('/user', user);
+    const PORT = 8081;
+    app.listen(PORT, () => {
+        console.log("Servidor rodando...")
+    })
+    app.use('/user', user);
--- routes/user.js
+++ routes/user.js
@@ -14,16 +14,34 @@ router.get('/new',(req, res)=>{
     
 })
 router.post('/add',(req, res)=>{
-    const novoToDo = {
-        nome: req.body.nome,
-        description: req.body.description
+    //validação
+    var erros = []
+
+    if(!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null){
+        erros.push({texto: "Invalid Name"})
+    }
+    if(!req.body.description || typeof req.body.description == undefined || req.body.description == null){
+        erros.push({texto: "Invalid Description"})
+    }
+    if(erros.length > 0){
+        res.render("user/add", {erros: erros})
+    }else{
+    //preenchendo formulário
+        const novoToDo = {
+            nome: req.body.nome,
+            description: req.body.description
+        }
+
+        new ToDo(novoToDo).save().then(()=>{
+            req.flash("success_msg", "Success to create new ToDo")
+            res.redirect("/user")
+        }).catch((err)=>{
+            req.flash("error_msg", "Error saving to do")
+            console.log("Erro ao cadastrar ToDo")
+        })
     }
 
-    new ToDo(novoToDo).save().then(()=>{
-        console.log("ToDo salvo com sucesso!")
-    }).catch((err)=>{
-        console.log("Erro ao cadastrar ToDo")
-    })
+    
     
 })
 
